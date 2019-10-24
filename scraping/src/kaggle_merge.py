3d2390eba61b9b9673a17dbd88d1f7e43d344a2f
Tim Yngesjö <timyn286@student.liu.se>
2019-10-24 13:59:33 -0700

    fixed bug in cleaning

--- scraping/src/kaggle_merge.py
+++ scraping/src/kaggle_merge.py
@@ -2,16 +2,19 @@ import csv
 from pprint import pprint
 from difflib import SequenceMatcher
 
-kaggle_path = "../../data/usa_companies.csv"
+kaggle_path = "../../data/companies_sorted.csv"
 glass_door_path = "../../data/glass_door.csv"
 
 
 data = {}
 
+first_kaggle = None #headers
+first_glass = None #headers
 
 i = 0
 with open(kaggle_path) as csvfile:
     reader = csv.reader(csvfile)
+    first_kaggle = next(reader)
     for row in reader:
         i+=1
         #if i > 10000:
@@ -29,6 +32,8 @@ result = {}
 label_file = open(glass_door_path)
 label_reader = csv.reader(label_file)
 labels = [row for row in label_reader]
+
+first_glass = labels[0]
 #    
 #for elem in data.keys():
 #    for label in labels:
@@ -37,21 +42,18 @@ labels = [row for row in label_reader]
 #            result[data[0], 0
 #            print(elem, label, sim_score)
 #
+used_names = set()
 for label in labels:
     s = label[0].strip().lower()
-    if label[0].strip().lower() in data:
+    if s in data and not s in used_names:
+        used_names.add(s)
         data[s] = data[s] + label[2:]
         result[s] = data[s]
-
-for key in result.keys():
-    print(result[key])
 
 
 outfile = open("out_merged.csv", "w")
 writer = csv.writer(outfile)
 
-
-print(result)
 
 
 def is_float(s):
@@ -74,13 +76,14 @@ for key in result.keys():
         continue
     
     for i, elem in enumerate(result[key]):
-        if not is_float(elem):
-            if is_float(elem[:-1]):
+        if is_float(elem[:-1]) and elem[-1] == "k":
                 result[key][i] = float(elem[:-1]) * 1000 
 
 for key in keys_to_remove:
     del result[key]         
         
 
+writer.writerow(first_kaggle + first_glass[2:])
+print(first_kaggle, first_glass)
 for key in result.keys():
     writer.writerow(result[key])
